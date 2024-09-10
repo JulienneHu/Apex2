add547447330ea43069e7a1d45347c0e09a6a83a
Jun_Hu <junhuuu@umich.edu>
2024-09-10 16:28:30 -0400

    noraxon update

---
+++ pages/Noraxon/CENSOR_ASSEMBLY.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title('Puttong on EMGs')
+
+st.subheader("Step1: Check all Necessary Components")
+st.image("assests/Noraxon/Necessary_Hardware.png", caption="Make Sure all Components Ready",width=600)
+
+st.subheader("Step2: Clean the Target Area")
+st.image("assests/Noraxon/clean.png", caption="Clean the Area with Alcohol Swap",width=600)
+
+st.subheader("Step3: Attach the Dual Electrodes")
+st.image("assests/Noraxon/Attach_Electrodes.png", caption="Attach the Dual Electrodes near the target area",width=600)
+
+st.subheader("Step4: Peel the Sticker off the EMG")
+st.image("assests/Noraxon/Peel_Sticker.png", caption="Peel the sticker off the EMG",width=600)
+
+st.subheader("Step5: Attach the EMG on the Target Area")
+st.image("assests/Noraxon/Attach_EMG.png", caption="Attach the EMG on the Target Area",width=600)
+
+st.subheader("Step6: Connect the EMG to the Electrodes")
+st.image("assests/Noraxon/Connect_EMG.png", caption="Connect the EMG to the Electrodes by Pinching the Smartleads",width=600)
