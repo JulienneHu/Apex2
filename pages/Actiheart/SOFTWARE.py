c31482ef532c92b94168ca714d8abf16cc065002
Jun_Hu <junhuuu@umich.edu>
2024-09-20 14:21:07 -0400

    IBI Updates

--- pages/Actiheart/SOFTWARE.py
+++ pages/Actiheart/SOFTWARE.py
@@ -48,14 +48,39 @@ st.image('assests/Actiheart/step14.png', caption='Data is being read', width=500
 st.subheader('Step15: Data Read Finish')
 st.image('assests/Actiheart/step15.png', caption='Data Read Successfully. Click “Finish”', width=500)
 
-st.subheader('Step16: View Recorded Data')
+st.subheader('Step16: Signal Test - View Recorded Data')
 st.image('assests/Actiheart/step16.png', caption='Double click on the recording you want to view', width=500)
 
-st.subheader('Step17: Scanning Data')
+st.subheader('Step17: Signal Test - Scanning Data')
 st.image('assests/Actiheart/step17.png', caption='Click “Scan All”', width=500)
 
-st.subheader('Step18: Access Info')
+st.subheader('Step18: Signal Test - Access Info')
 st.image('assests/Actiheart/step18.png', caption='Click “Info” in the "Signal Test"', width=500)
 
-st.subheader('Step19: Export Data')
+st.subheader('Step19: Signal Test - Export Data')
 st.image('assests/Actiheart/step19.png', caption='Ensure “Comma delimited” is selected so that the file is in csv format', width=500)
+
+st.subheader('Step20: IBI Monitoring')
+st.image('assests/Actiheart/step20.png', caption='Click “IBI Monitoring” in the Main Menu', width=500)
+
+st.subheader('Step21: IBI Monitoring - Click "Setup"')
+st.image('assests/Actiheart/step21.png', caption='Click “Setup”', width=500)
+
+st.subheader('Step22: IBI Monitoring - Find Existing User')
+st.image('assests/Actiheart/step22.png', caption='Click “Find”', width=500)
+
+st.subheader('Step23: IBI Monitoring - Select User')
+st.image('assests/Actiheart/step23.png', caption='Pick your User ID then Click "Next"', width=500)
+
+st.subheader('Step24: IBI Monitoring - Make sure the Actiheart is connected')
+st.image('assests/Actiheart/step24before.png', caption='Before you connect Actiheart to computer', width=500)
+st.image('assests/Actiheart/step24after.png', caption='After connecting Actiheart to computer, click “Send”', width=500)
+
+st.subheader('Step25: IBI Monitoring -  Data Collection')
+st.image('assests/Actiheart/step25.png', caption='Unplug Actiheart Immediately and Start Data Collection', width=500)
+
+st.subheader('Step26: IBI Monitoring -  Read Data')
+st.image('assests/Actiheart/step26.png', caption='When Collection Finished, Remove Actiheart from Participant and replug to then Computer.Then Click “Read” in "IBI Monitoring" to read the data.', width=500)
+
+st.subheader('Step27: IBI Monitoring -  Save Data')
+st.image('assests/Actiheart/step27.png', caption='Ensure “Comma delimited” is selected so that the file is in csv format', width=500)
